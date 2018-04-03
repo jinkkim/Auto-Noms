5c01c1bba862d019624a0667177aa57261ac9191
Jin <celine.jkim@gmail.com>
2018-04-03 00:40:56 -0400

    implant google places API in quiz.html and app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,89 +1,88 @@
 $(document).ready(function() {
+    var appKey = "AIzaSyBTaTIP0pf27QBpsg3ofYZcc3ZLhv1RMeU";
+    var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
 
-    $("#clear").click(function() {
-        $("#figure").attr("value","");
-        $("#min_year").attr("value","");
-        $("#max_year").attr("value","");
-        $("#figure").val("");
-        $("#min_year").val("");
-        $("#max_year").val("");
-    });
+    var keyword = "";
+    var zipcode = "";
+    var zipDB = "";
+    var location = "";      //latitude,longitude
+    var radius = "";
+    var minprice = "";
+    var maxprice = "";
 
-    $("#figure").click(function(){
-        $("#figure").attr("value","");
-    });
+    $("#submit").click(function(event) {
 
-    $("#min_year").click(function(){
-        $("#min_year").attr("value","");
-    });
+        event.preventDefault();
+        $("#query-result").empty();
 
-    $("#max_year").click(function(){
-        $("#max_year").attr("value","");
-    });
+        //location (required)
+        zipcode = $("#zipcode").val().trim();
 
-
-    $("#submit").click(function() {
-
-
-        var parameters = {
-            'api-key' : "AIzaSyBTaTIP0pf27QBpsg3ofYZcc3ZLhv1RMeU",
-        };
-
-
-        if( $("#keyword").val() == "Enter Keyword" || $("#keyword").val() == "" ) {
-            return
-        } else {
-            parameters['q'] =  $("#keyword").val();
+        function getZipDB() {
+            var value = $.ajax({
+                url: 'assets/db/codes.json',
+                async: false,
+            }).responseText;
+            return value;
         }
 
+        if (zipcode.length == 5 && Number.isInteger(parseInt(zipcode))){
+            zipDB = JSON.parse(getZipDB());
+            location = zipDB[zipcode].latitude + ',' + zipDB[zipcode].longitude;
+            //queryURL = queryURL + "location=" + location;
+            queryURL += "location=" + location;
+        } else {
+            alert("Please only input a valid zipcode.");
+        }
         
-        if( $("location").val() == "zipcode or city" || $("#location").val() == "") {
-            return
-        } else {
-            parameters['location'] =  $("#location").val();
-        }
 
+        // radius (required)
+        radius = parseInt($("#radius").val().trim()) * 1609.344; //convert miles into meters
 
-       if( $("#radius").val() == "miles" || $("#radius").val() == "") {
-            return
-        } else {
-            parameters['radius'] =  $("#radius").val();
-        }
-
-        console.log(parameters)
-
-
-        var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters";
-        queryURL += '?' + $.param(parameters);
-
+        var radius_string = radius.toString();
+        queryURL += "&radius=" + radius_string; 
         
+        // type = restaurant
+        queryURL += "&type=restaurant"; 
+
+        // keyword (optional)
+        keyword = $("#keyword").val().trim();
+        if (keyword) {
+            queryURL += "&keyword=" + keyword;
+        }
+
+        //min price and max price
+        minprice = $("#minPrice").val();
+        maxprice = $("#maxPrice").val();
+        if (minprice) {
+            queryURL += "&minprice=" + minprice; 
+        }
+        
+        if (maxprice) {
+            queryURL += "&maxprice=" + maxprice;
+        }
+
+        //open now
+        var n = $("input:checked").length;
+        if(n==1) {
+            queryURL += "&opennow"
+        }
+
+        //api key
+        queryURL += "&key=" + appKey;
+        console.log(queryURL);
+
+        //test url
+        //queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=33.7718,-84.3757&radius=8046.72&type=restaurant&keyword=hamburger&minprice=2&maxprice=4&opennow&key=AIzaSyBTaTIP0pf27QBpsg3ofYZcc3ZLhv1RMeU";
+
         $.ajax({
             url: queryURL,
-            method: "GET"
-        }).then(function(result) {
+            method: 'GET'
+        }).done(function(result){
             console.log(result);
-
-            // DOM modifiers
-            print(result);
-
-        });     
+            //todo
+        })
+         
     });
-
-    
-    function print(result){
-    // console.log(docsdisplayed);
-    var articles = "";
-    for (var i=0; i< 10; i++){
-        articles += "<a class='m-1 p-1 text-primary' href=" + result.response.docs[i].web_url + ">" +
-                    "<p><b>" + result.response.docs[i].headline.main + "</b> <br>" +
-                     result.response.docs[i].snippet + "<br>" +
-                    "Publication date: " + result.response.docs[i].pub_date.substring(0,10) + "</p></a><br>" ;      
-    }
-
-    $("#results").html(articles);
-    }
-
-
-
 
 })
